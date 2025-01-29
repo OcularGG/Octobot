ae100606bad9b4e3d9924ff95a79500ef1934823
Quasain <ashviknacham@gmail.com>
2025-01-28 17:36:06 -0800

    react.js

--- react.js
+++ react.js
@@ -1,13 +1,9 @@
-const TARGET_CHANNEL_ID = '1100847035013419114';
-
-const TARGET_EMOJI = '<:Ocular:1324391933635727380>';
-
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
 
-    if (message.channel.id === TARGET_CHANNEL_ID) {
+    if (message.channel.id === 1100847035013419114) {
         try {
-            await message.react(TARGET_EMOJI);
+            await message.react(<:Ocular:1324391933635727380>);
         }
     }
 });
