82b46bbccfa23990ab5bf634f57d50534b75c19a
Quasain <ashviknacham@gmail.com>
2025-01-28 17:46:17 -0800

    blank for now

--- react.js
+++ react.js
@@ -1,9 +1,1 @@
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
 
-    if (message.channel.id === 1100847035013419114) {
-        try {
-            await message.react(<:Ocular:1324391933635727380>);
-        }
-    }
-});
